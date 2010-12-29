f340b39eabdd74e00894891c8475107784f0bf03
btamiller <btamiller@22cb880c-9e46-0410-a028-d5b2de49818f>
2010-12-29 22:49:11 +0000

    Finish writing card I/O, now need to fix the atom lib to take card format
    git-svn-id: https://charmming.googlecode.com/svn/charmminglib@128 22cb880c-9e46-0410-a028-d5b2de49818f

--- src/charmming/io/crd.py
+++ src/charmming/io/crd.py
@@ -19,7 +19,7 @@ def get_formatting(input):
     """
     Takes a string representing the location of a .crd file or an
     iterator of strings as input, and returns a string indicating the
-    formatting of the pdb data: "short", "long" or "unknown".
+    formatting of the pdb data: "shortcard", "longcard" or "unknown".
     """
     try:
         iterator = ( line.lower() for line in open(input) if
@@ -42,9 +42,9 @@ def get_formatting(input):
     if badlen:
         return 'unknown'
     if short and not long:
-        return 'short'
+        return 'shortcard'
     if long and not short:
-        return 'long'
+        return 'longcard'
     return 'unknown'
 
 
@@ -52,7 +52,6 @@ class CRDFile(object):
     """
     Class Attributes
         `_autoInFormat`
-        `_reCode`
     Public Attributes
         `warnings`
     Properties
@@ -71,39 +70,27 @@ class CRDFile(object):
     Private Methods
         _get_formatting
         _partitions
-        _fix_chainids
-        _fix_resids     TODO
         _build_models
     Special Methods
     """
 
-    _autoInFormat = 'short'
+    _autoInFormat = 'shortcard'
     """
     A string that defines the default input formatting for all class
     instances.
     """
 
-    _reCode = re.compile(r"(?<![a-z0-9])[0-9][a-z0-9]{3}(?![a-z0-9])")
-    """
-    A regular expression for deriving the PDB code from the filename of
-    a .pdb file named using CHARMMing conventions.
-    """
-
     def __init__(self, filename, **kwargs):
         """
         kwargs:
-            `informat`      ['auto','pdborg','charmm']
-            `fix_chainid`   [True,False]    # Attempts to fix mangled chainid
-            `fix_resid`     [True,False]    # Attempts to fix mangled resid
-            `autofix`       [True,False]    # Flag for atom._autoFix
+            `informat`      ['auto','shortcard','longcard']
+            `autofix`       [False,True]    # Flag for atom._autoFix
             `verbose`       [False,True]
         """
         # kwargs
         kwargs = lowerKeys(kwargs)
         inFormat = kwargs.pop('informat', 'auto')
-        fix_chainid = kwargs.pop('fix_chainid', True)
-        fix_resid = kwargs.pop('fix_resid', True)
-        self._autoFix = kwargs.pop('autoFix', True)
+        self._autoFix = kwargs.pop('autoFix', False)
         self._verbose = kwargs.pop('verbose', False)
         #
         self.warnings = []
@@ -120,47 +107,19 @@ class CRDFile(object):
             self._inFormat = inFormat
         if self._verbose:
             print '%s: Input formatting set to `%s`' % (self.code, self.inFormat)
-        # auto fixing
-        if fix_chainid:
-            if self._verbose:
-                print '%s: Fixing `chainid`s' % self.code
-            self._fix_chainids()
-        #TODO Implement fix_resids
-        if fix_resid:
-            if self._verbose:
-                print '%s: Fixing `resid`s' % self.code
-            #self._fix_resids()
-        # Processing
-        self._models = {}
-        self._build_models()
+        # Processing -- this is simpler than the PDB case because we
+        # do not have multiple models
+        self._build_crd()
 
 ##############
 # Properties #
 ##############
 
     @Property
-    def code():
-        doc =\
-        """
-        The PDB acquisition code of the `PDBFile`.
-        """
-        def fget(self):
-            tmp = [ line for line in self.header if line.startswith('header') ]
-            if tmp:
-                return tmp[0].split()[-1]
-            else:
-                searchThis = os.path.basename(self.filename).lower()
-                try:
-                    return PDBFile._reCode.findall(searchThis)[0]
-                except IndexError:
-                    return '????'
-        return locals()
-
-    @Property
     def crd():
         doc =\
         """
-        The atomic coordinate data from the .pdb file, in its
+        The atomic coordinate data from the .crd file, in its
         unadulterated text form.
         """
         def fget(self):
@@ -171,7 +130,7 @@ class CRDFile(object):
     def filename():
         doc =\
         """
-        The name of the .pdb file from which this `PDBFile` instance
+        The name of the .crd file from which this `CRDFile` instance
         was created.
         """
         def fget(self):
@@ -181,21 +140,10 @@ class CRDFile(object):
         return locals()
 
     @Property
-    def footer():
-        doc =\
-        """
-        The metadata in the .pdb file that follows the atomic coordinate
-        data.  This property gives access to its unadulterated text form.
-        """
-        def fget(self):
-            return self._footer
-        return locals()
-
-    @Property
     def header():
         doc =\
         """
-        The metadata in the .pdb file that precedes the atomic
+        The metadata in the .crd file that precedes the atomic
         coordinate data.  This property gives access to its
         unadulterated text form.
         """
@@ -207,7 +155,7 @@ class CRDFile(object):
     def inFormat():
         doc =\
         """
-        The formatting of the input data: "pdborg" or "charmm".
+        The formatting of the input data: "shortcard" or "longcard".
         """
         def fget(self):
             return self._inFormat
@@ -217,7 +165,7 @@ class CRDFile(object):
     def path():
         doc =\
         """
-        The path of the .pdb file from which this `PDBFile` instance
+        The path of the .crd file from which this `CRDFile` instance
         was created.
         """
         def fget(self):
@@ -245,13 +193,6 @@ class CRDFile(object):
         """
         return itertools.chain(self.iter_models())
 
-    def iter_models(self):
-        """
-        Iterate over the PDBFile's models. Models are Mol objects
-        derived directly from the original pdb file.
-        """
-        return ( self['model%d' % i] for i in sorted(self._models.keys()) )
-
     def keys(self):
         """
         Lists the keys to access all of the Mol objects the PDBFile
@@ -264,63 +205,21 @@ class CRDFile(object):
 # Private Methods #
 ###################
 
-    def _build_models(self):
+    def _build_crd(self):
         """
         Parse the crd section, and load the coordinates into `Mol`
         objects, one `Mol` object per model section in the .pdb file.
         """
-        models = paragraphs(self.crd, splitter=['model'])
-        for model in models:
-            if model[0].startswith('model'):
-                modelNum = int(model[0].split()[1])
-            else:
-                modelNum = 0
-            iterator = ( Atom(text=line, informat=self.inFormat, index=i,
-                        autofix=self._autoFix) for i, line in enumerate(model)
-                        if line.startswith(('atom', 'hetatm')) )
-            self._models[modelNum] = Mol(iterable=iterator, name='model%d' %
-                                        modelNum, code=self.code, autofix=True)
 
-    def _fix_chainids(self):
-        """
-        Detect and correct chainid mangling.
-        """
-        # Detect mangling
-        def gen():
-            for line in self.crd:
-                if line.startswith(('atom', 'hetatm')):
-                    tmp = Atom(text=line, informat=self.inFormat, autofix=False)
-                else:
-                    continue
-                yield tmp.chainid
-        # Correct mangling
-        if '' in set(gen()):
-#WARN       # Throw a warning if chainids are mangled
-            self.warnings.append('chainids mangled')
-            print 'One or more chainids appear to be missing, attempting to\
-                    autofix.\n'
-            print 'Please verify the accuracy of your .pdb file upon\
-                    completion.\n'
-            chainNum = 0
-            for i,line in enumerate(self.crd):
-                if line.startswith(('atom', 'hetatm')):
-                    tmp = Atom(text=line, informat=self.inFormat, autofix=False)
-                    # Blank chainid
-                    if not tmp.chainid:
-                        tmp.chainid = alphanum[chainNum]
-                        self.crd[i] = tmp.Print(outformat=self.inFormat).lower()
-                elif line.startswith('ter'):
-                    chainNum += 1
-                elif line.startswith('model'):
-                    chainNum = 0
-                else:
-                    continue
+        if self.inFormat == 'shortcard':
+            iterator = ( Atom(text=line, informat='shortcard', index=i,
+                        autofix=self._autoFix) for i, line in enumerate(self.crd) if len(line) == 70 )
+        elif self.inFormat == 'longcard':
+            iterator = ( Atom(text=line, informat='longcard', index=i,
+                        autofix=self._autoFix) for i, line in enumerate(self.crd) if len(line) == 140 )
 
-    def _fix_resids(self):
-        """
-        Detect and correct resid mangling.
-        """
-        raise NotImplementedError
+        self._models[modelNum] = Mol(iterable=iterator, name='crdmol',
+                                     autofix=False)
 
     def _get_formatting(self, filename):
         """
@@ -329,7 +228,7 @@ class CRDFile(object):
         result = get_formatting(filename)
         if result == 'unknown':
 #WARN       # Throw a warning if formatting is guessed.
-            self.warnings.append('Undetected pdb formatting')
+            self.warnings.append('Undetected crd formatting')
             return self.__class__._autoInFormat
         else:
             return result
@@ -341,33 +240,15 @@ class CRDFile(object):
         filePointer = open(self.filename)
         # Populate header
         self._header = []
+        self._crd = []
         tmp = None
         for line in cleanStrings(filePointer):
-            if line.startswith(('atom', 'hetatm', 'model')):
-                tmp = line
-                break
-            else:
+            if line.startswith(('*')) or len(line) < 30:
                 self._header.append(line)
-        # Populate coordinates
-        if tmp is None:
-            self._crd = []
-        else:
-            self._crd = [tmp]
-        tmp = None
-        for line in cleanStrings(filePointer):
-            if not line.startswith(('atom', 'anisou', 'hetatm', 'model', 'ter',
-                                    'endmdl')):
-                tmp = line
-                break
             else:
                 self._crd.append(line)
-        # Populate footer
-        if tmp is None:
-            self._footer = []
-        else:
-            self._footer = [tmp]
-        for line in cleanStrings(filePointer):
-            self._footer.append(line)
+
+        self._footer = []
         filePointer.close()
 
 ###################
