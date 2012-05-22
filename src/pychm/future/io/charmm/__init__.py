5cebe76fa505b8b1b8f2ea9daeca56a7fcd097f4
pickard81 <pickard81@22cb880c-9e46-0410-a028-d5b2de49818f>
2012-05-22 23:31:07 +0000

    oops comma
    git-svn-id: https://charmming.googlecode.com/svn/charmminglib@372 22cb880c-9e46-0410-a028-d5b2de49818f

--- src/pychm/future/io/charmm/__init__.py
+++ src/pychm/future/io/charmm/__init__.py
@@ -3,6 +3,5 @@
 import pychm.future.io.charmm.base
 from pychm.future.io.charmm.dcd import *
 from pychm.future.io.charmm.prm import *
-from pychm.future.io.charmm.rtf import *
 
-__all__ = ["dcd", "prm", "rtf"]
+__all__ = ["dcd", "prm"]
